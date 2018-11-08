c611247d22305f98d3ea90c744a6dab558b1ed99
Matthew Lin <mlin367@yahoo.com>
2018-11-07 19:47:57 -0800

    Main proxy

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, '../dist/')));
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`proxy listening at port ${port}`);
+});
